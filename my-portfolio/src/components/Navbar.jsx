f1ad99047798adc8597e653c27562d9e0be05c78
Danielle <daniellelee73@gmail.com>
2024-11-02 22:27:46 +0000

    Created Navbar component and Homepage

---
+++ my-portfolio/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+//import React from "react";
+
+const Navbar = () => {
+    return ( 
+        <>
+
+        <div className="NavContain">
+            <a href="#Home">Home</a>
+            <a href="#About">About</a>
+            <a href="#Contact">Contact</a>
+        </div>
+        
+        
+        </>
+     );
+}
+ 
+export default Navbar;
---
+++ my-portfolio/src/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Homepage = () => {
+    return ( 
+        <>
+        <h1>Hello this is a test</h1>
+        </>
+     );
+}
+ 
+export default Homepage;
