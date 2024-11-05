da5968c04ace57fde168e752aeb48bc3bfaa1414
Danielle <daniellelee73@gmail.com>
2024-11-05 11:21:08 +0000

    Added scroll functionality to the page for the navbar

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,70 +1,67 @@
-import Navbar from './components/Navbar'
-import Homepage from './pages/Homepage'
-import ProjectCard from './components/ProjectCard'
-import Footer from './components/Footer'
+import Navbar from './components/Navbar';
+import Homepage from './pages/Homepage';
+import ProjectCard from './components/ProjectCard';
+import Footer from './components/Footer';
 
-
-import './App.css'
+import './App.css';
 
 function App() {
   return (
-    <>  
-    <Navbar />
+    <>
+      {/* Home Section */}
+      <div id="home"></div>
 
-    {/* projects section */}
+      <Navbar />
 
-    <div id="Projects" className="text-center">
-       <h2 className="relative inline-block pb-2 mt-10 mb-4">
+      {/* Projects Section */}
+      <div id="projects" className="text-center">
+        <h2 className="relative inline-block pb-2 mt-10 mb-4">
           <span>Projects:</span>
-        <span 
-            className="absolute bottom-0 left-0 w-full h-2" // h-2 makes it thicker
-            style={{ backgroundColor: '#FDC435' }}></span>
+          <span
+            className="absolute bottom-0 left-0 w-full h-2"
+            style={{ backgroundColor: '#FDC435' }}
+          ></span>
         </h2>
-    </div>
-    
-    <ProjectCard 
-    title="My Project"
-    description="This is my awesome project"
-    imageUrl="/project-image.jpg"
-    repoUrl="https://github.com/myproject"
-    />
-    
-    <ProjectCard 
-    title="My Project"
-    description="This is my awesome project"
-    imageUrl="/project-image.jpg"
-    repoUrl="https://github.com/myproject"
-    />
-    
+        <ProjectCard
+          title="My Project"
+          description="This is my awesome project"
+          imageUrl="/project-image.jpg"
+          repoUrl="https://github.com/myproject"
+        />
+        <ProjectCard
+          title="My Project"
+          description="This is my awesome project"
+          imageUrl="/project-image.jpg"
+          repoUrl="https://github.com/myproject"
+        />
+      </div>
 
-    {/* About section */}
-
-    <div id="About" className="text-center">
-       <h2 className="relative inline-block pb-2 mt-10 mb-4">
+      {/* About Section */}
+      <div id="about" className="text-center">
+        <h2 className="relative inline-block pb-2 mt-10 mb-4">
           <span>About:</span>
-        <span 
-            className="absolute bottom-0 left-0 w-full h-2" // h-2 makes it thicker
-            style={{ backgroundColor: '#FDC435' }}></span>
+          <span
+            className="absolute bottom-0 left-0 w-full h-2"
+            style={{ backgroundColor: '#FDC435' }}
+          ></span>
         </h2>
-    </div>
-    <Homepage />
+        <Homepage />
+      </div>
 
-
-{/* Contact Section */}
-
-    <div id="Contact" className="text-center">
-       <h6 className="relative inline-block pb-2 mt-10 mb-4">
+      {/* Contact Section */}
+      <div id="contact" className="text-center">
+        <h6 className="relative inline-block pb-2 mt-10 mb-4">
           <span>Contact:</span>
-        <span 
-            className="absolute bottom-0 left-0 w-full h-2" // h-2 makes it thicker
-            style={{ backgroundColor: '#FDC435' }}></span>
+          <span
+            className="absolute bottom-0 left-0 w-full h-2"
+            style={{ backgroundColor: '#FDC435' }}
+          ></span>
         </h6>
-    </div>
+      </div>
 
-    <Footer />
+      <Footer />
     </>
-   
-  )
+  );
 }
 
-export default App
+export default App;
--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -1,39 +1,40 @@
-//import React from "react";
+// Navbar.js
 import ProfilePicture from '../assets/ProfilePicture.png';
 
 const Navbar = () => {
-    return ( 
-        <>
+  return ( 
+    <>
+      <div className="NavContainer flex justify-evenly shadow-md">
+        {/* Home Link */}
+        <a href="#home" className="mr-96 hover:bg-yellow-400">Danielle Lee</a>
 
-        <div className="NavContainer flex justify-evenly shadow-md">
-            <a href="#Home" className="mr-96 hover:bg-yellow-400">Danielle Lee</a>
-            <a href="#About" className="px-8 hover:bg-yellow-400">About</a>
-            <a href="#Projects" className="px-8 hover:bg-yellow-400">Projects</a>
-            <a href="#Contact" className="px-8 hover:bg-yellow-400">Contact</a>
+        {/* Other Links */}
+        <a href="#about" className="px-8 hover:bg-yellow-400">About</a>
+        <a href="#projects" className="px-8 hover:bg-yellow-400">Projects</a>
+        <a href="#contact" className="px-8 hover:bg-yellow-400">Contact</a>
+      </div>
+
+      <div className="TopContainer flex justify-evenly mt-28 mb-28">
+        <div className="LeftContainer m-8">
+          <h2>Hello I&apos;m Danielle</h2>
+          <div className="aboutMeParagraph text-4xl"> 
+            <p>I&apos;m a Front-End Developer <br /> based in London.</p>
+            <p>Nice to meet you!</p>
+          </div>
+          <br />
+          <button>C.V.</button> <button>Contact</button>
         </div>
-
-        <div className="TopContainer flex justify-evenly mt-28 mb-28">
-            <div className="LeftContainer m-8">
-                <h2>Hello I&apos;m Danielle</h2>
-                <div className="aboutMeParagraph text-4xl"> 
-                <p>I&apos;m a Front-End Developer <br /> based in London.</p>
-                <p>Nice to meet you!</p>
-                </div>
-                <br />
-                <button>C.V.</button> <button>Contact</button>
-            </div>
-            <div className="RightContainer m-8">
-            <img 
+        <div className="RightContainer m-8">
+          <img 
             src={ProfilePicture} 
             alt="Profile Picture" 
             className="rounded-full border-8 border-yellow-400"
-            width={350}/>
-            </div>
+            width={350}
+          />
         </div>
-        
-        
-        </>
-     );
-}
- 
+      </div>
+    </>
+  );
+};
+
 export default Navbar;
