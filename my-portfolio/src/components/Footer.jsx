a5e720648cc83c253f8b0d7d2294fd56f2372666
Danielle <daniellelee73@gmail.com>
2024-11-05 00:42:10 +0000

    style changes

--- my-portfolio/src/components/Footer.jsx
+++ my-portfolio/src/components/Footer.jsx
@@ -1,9 +1,15 @@
-import { FaCode, FaLinkedin } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { SiCodewars } from "react-icons/si";
 
 const Footer = () => {
+
+const currentYear = new Date().getFullYear(); 
+
+
     return ( 
+
+        <>
         <div className="footer flex flex-row justify-center space-x-4 m-5">
             {/* LinkedIn Icon */}
             <a href="https://www.linkedin.com/in/danielle-lee-ab8b0887/" target="_blank" rel="noopener noreferrer">
@@ -19,8 +25,11 @@ const Footer = () => {
              <a href="https://www.linkedin.com/in/danielle-lee-ab8b0887/" target="_blank" rel="noopener noreferrer">
                 <SiCodewars size={32} color="black" /> {/* Adjust size and color as needed */}
             </a>
-            
         </div>
+
+        <p className="flex justify-center text-sm" >Danielle Lee {currentYear}</p>
+
+        </>
     );
 }
  
