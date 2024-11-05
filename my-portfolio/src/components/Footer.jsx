982b1889412446207d6288b55663d5778528f118
Danielle <daniellelee73@gmail.com>
2024-11-05 02:14:41 +0000

    styles modified to body (font colors, and navbar.) picture asset added

--- my-portfolio/src/components/Footer.jsx
+++ my-portfolio/src/components/Footer.jsx
@@ -13,17 +13,17 @@ const currentYear = new Date().getFullYear();
         <div className="footer flex flex-row justify-center space-x-4 m-5">
             {/* LinkedIn Icon */}
             <a href="https://www.linkedin.com/in/danielle-lee-ab8b0887/" target="_blank" rel="noopener noreferrer">
-                <FaLinkedin size={32} color="black" /> {/* Adjust size and color as needed */}
+                <FaLinkedin size={32} color="#4e4b4b" /> {/* Adjust size and color as needed */}
             </a>
 
             {/* github */}
-            <a href="https://www.linkedin.com/in/danielle-lee-ab8b0887/" target="_blank" rel="noopener noreferrer">
-                <FaGithub size={32} color="black" /> {/* Adjust size and color as needed */}
+            <a href="https://github.com/Danielle73/" target="_blank" rel="noopener noreferrer">
+                <FaGithub size={32} color="#4e4b4b" /> {/* Adjust size and color as needed */}
             </a>
 
              {/* codewars */}
              <a href="https://www.linkedin.com/in/danielle-lee-ab8b0887/" target="_blank" rel="noopener noreferrer">
-                <SiCodewars size={32} color="black" /> {/* Adjust size and color as needed */}
+                <SiCodewars size={32} color="#4e4b4b" /> {/* Adjust size and color as needed */}
             </a>
         </div>
 
