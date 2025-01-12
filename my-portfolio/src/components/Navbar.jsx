a5a1df63b83a1c23751356223d58e89bd954991e
Danielle <daniellelee73@gmail.com>
2025-01-12 18:21:15 +0000

    Style changes made, icons added to navbar

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -1,5 +1,11 @@
 // Navbar.js
 import ProfilePicture from '../assets/ProfilePicture.png';
+import { FaRegFolder } from "react-icons/fa6";
+import { FaRegAddressCard } from "react-icons/fa";
+import { FaRegEnvelope } from "react-icons/fa6";
+
+
+
 
 const Navbar = () => {
   return ( 
@@ -9,9 +15,17 @@ const Navbar = () => {
         <a href="#home" className="mr-96 hover:bg-yellow-400">Danielle Lee</a>
 
         {/* Other Links */}
-        <a href="#about" className="px-8 hover:bg-yellow-400">About</a>
-        <a href="#projects" className="px-8 hover:bg-yellow-400">Projects</a>
-        <a href="#contact" className="px-8 hover:bg-yellow-400">Contact</a>
+        <a href="#about" className="px-8 hover:bg-yellow-400 flex items-center gap-2">
+          <FaRegAddressCard className="text-lg" />About</a>
+
+
+        <a href="#projects" className="px-8 hover:bg-yellow-400 flex items-center gap-2">
+          <FaRegFolder className="text-lg" />Projects</a>
+
+        <a href="#contact" className="px-8 hover:bg-yellow-400 flex items-center gap-2"><FaRegEnvelope className="text-lg"/>Contact</a>
+
+        
+
       </div>
 
       <div className="TopContainer flex justify-evenly mt-28 mb-28">
