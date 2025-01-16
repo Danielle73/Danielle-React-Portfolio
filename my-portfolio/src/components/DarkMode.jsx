8e27054f455eba06d8595645bb8715734e8a87d0
Danielle73 <daniellelee73@gmail.com>
2025-01-16 20:41:34 +0000

    Created a darktheme component and dark theme css

---
+++ my-portfolio/src/components/DarkMode.jsx
@@ -0,0 +1,17 @@
+import 'my-portfolio/src/DarkTheme.css'
+
+function DarkTheme(){
+    return (
+        <div className="toggle-theme-wrapper">
+            <span>☀️</span>
+            <label className='toggle-theme' htmlFor='checkox'>
+                <input type='checkbox'
+                id='checkbox'/>
+                <div className="slider-round"></div>
+            </label>
+            <span>🌒</span>
+        </div>
+    )
+}
+
+export default DarkTheme;
