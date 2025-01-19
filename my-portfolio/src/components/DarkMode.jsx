9f81e8e07e12a4386e9eadca706121a3c61720b2
Danielle73 <daniellelee73@gmail.com>
2025-01-19 22:35:01 +0000

    Css changes for checkbox changed to slider

--- my-portfolio/src/components/DarkMode.jsx
+++ my-portfolio/src/components/DarkMode.jsx
@@ -2,16 +2,18 @@ import '../DarkTheme.css';
 
 function DarkTheme(){
     return (
-        <div className="toggle-theme-wrapper flex items-center gap-2 px-8">
-            <span>☀️</span>
-            <label className='toggle-theme' htmlFor='checkox'>
-                <input type='checkbox'
-                id='checkbox'/>
-                <div className="slider-round"></div>
-            </label>
-            <span>🌒</span>
+        <div className="toggle-theme-wrapper">
+          <span>☀️</span>
+          <label className="toggle-theme" htmlFor="checkbox">
+            <input
+              type="checkbox"
+              id="checkbox"
+            />
+            <div className="slider round"></div>
+          </label>
+          <span>🌒</span>
         </div>
-    )
+      );
 }
 
 export default DarkTheme;
