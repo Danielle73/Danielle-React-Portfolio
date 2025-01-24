b130eff9a5a0bbcb34a39c1083f92d0cbb25fd55
Danielle73 <daniellelee73@gmail.com>
2025-01-24 19:49:13 +0000

    Put projects grid into its own component

---
+++ my-portfolio/src/components/ProjectsGrid.jsx
@@ -0,0 +1,21 @@
+<div id="projects" className="text-center">
+        <h2 className="relative inline-block pb-2 mt-10 mb-4">
+          <span>Projects:</span>
+          <span
+            className="absolute bottom-0 left-0 w-full h-2"
+            style={{ backgroundColor: '#FDC435' }}
+          ></span>
+        </h2>
+        <ProjectCard
+          title="Simon Game"
+          description="A color memory game"
+          imageUrl="..my-portfolio/src/assets/SimonGame.png"
+          repoUrl="https://github.com/FAC-community/summer2024/tree/main/users/Danielle%26JackGame/Simon%20game%20Vanilla"
+        />
+        <ProjectCard
+          title="My Project"
+          description="This is my awesome project"
+          imageUrl="/project-image.jpg"
+          repoUrl="https://github.com/myproject"
+        />
+      </div>
