4cbe4e3af1c28c48bd5acd23f048177c695f09d2
Danielle73 <daniellelee73@gmail.com>
2025-01-25 22:39:13 +0000

    Created AboutSection component & rendered in app

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,10 +1,10 @@
 import Navbar from './components/Navbar';
 import MainBody from './components/MainBody';
 import ProjectsGrid from './components/ProjectsGrid';
-
+import AboutSection from './components/AboutSection';
 // import Homepage from './pages/Homepage';
 // import ProjectCard from './components/ProjectCard';
-// import Footer from './components/Footer';
+import Footer from './components/Footer';
 
 import './App.css';
 
@@ -17,34 +17,9 @@ function App() {
 
       <ProjectsGrid />
 
+      <AboutSection />     
 
-      {/* Projects Section */}
-      {/* Import projects grid component here */}
-
-      {/* About Section */}
-      {/* <div id="about" className="text-center">
-        <h2 className="relative inline-block pb-2 mt-10 mb-4">
-          <span>About:</span>
-          <span
-            className="absolute bottom-0 left-0 w-full h-2"
-            style={{ backgroundColor: '#FDC435' }}
-          ></span>
-        </h2>
-        <Homepage />
-      </div> */}
-
-      {/* Contact Section */}
-      {/* <div id="contact" className="text-center">
-        <h6 className="relative inline-block pb-2 mt-10 mb-4">
-          <span>Contact:</span>
-          <span
-            className="absolute bottom-0 left-0 w-full h-2"
-            style={{ backgroundColor: '#FDC435' }}
-          ></span>
-        </h6>
-      </div> */}
-
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
---
+++ my-portfolio/src/components/AboutSection.jsx
@@ -0,0 +1,32 @@
+import Homepage from "../pages/Homepage";
+
+function AboutSection(){
+    return(
+        <> 
+        <div id="about" className="text-center">
+        <h2 className="relative inline-block pb-2 mt-10 mb-4">
+          <span>About:</span>
+          <span
+            className="absolute bottom-0 left-0 w-full h-2"
+            style={{ backgroundColor: '#FDC435' }}
+          ></span>
+        </h2>
+        <Homepage />
+      </div>
+
+
+{/* Contact section */}
+<div id="contact" className="text-center">
+<h6 className="relative inline-block pb-2 mt-10 mb-4">
+  <span>Contact:</span>
+  <span
+    className="absolute bottom-0 left-0 w-full h-2"
+    style={{ backgroundColor: '#FDC435' }}
+  ></span>
+</h6>
+</div>
+</>
+    )
+}
+
+export default AboutSection;
