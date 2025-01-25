30fb1b329f85febdcacf20e0a50e5bba9b415626
Danielle73 <daniellelee73@gmail.com>
2025-01-25 22:40:31 +0000

    Minor code changes to clean up app component

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -2,8 +2,6 @@ import Navbar from './components/Navbar';
 import MainBody from './components/MainBody';
 import ProjectsGrid from './components/ProjectsGrid';
 import AboutSection from './components/AboutSection';
-// import Homepage from './pages/Homepage';
-// import ProjectCard from './components/ProjectCard';
 import Footer from './components/Footer';
 
 import './App.css';
