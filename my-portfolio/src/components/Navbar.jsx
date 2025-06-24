d187fcf84dd302a3115998acf2fbe9344bdc6df2
Danielle Lee <daniellelee73@gmail.com>
2025-06-24 17:36:35 +0100

    Added blog path and blog icon to nav bar

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 // Navbar.js
 //import ProfilePicture from '../assets/ProfilePicture.png';
-import { FaRegFolder } from "react-icons/fa6";
+import { FaBlogger, FaRegFolder } from "react-icons/fa6";
 import { FaRegAddressCard } from "react-icons/fa";
 import { FaRegEnvelope } from "react-icons/fa6";
 import { BsHouseHeart } from "react-icons/bs";
@@ -24,6 +24,9 @@ const Navbar = () => {
 
         <a href="#contact" className="relative px-4 py-3 after:block after:absolute after:bottom-0 after:left-1/2 after:w-0 after:h-0.5 after:bg-yellow-400 after:transition-all after:duration-300 hover:after:w-full hover:after:left-0 flex items-center gap-2">          
           <FaRegEnvelope className="text-lg"/>Contact</a>
+
+        <a href="https://accessibility-blog.vercel.app/" target="_blank" className="relative px-4 py-3 after:block after:absolute after:bottom-0 after:left-1/2 after:w-0 after:h-0.5 after:bg-yellow-400 after:transition-all after:duration-300 hover:after:w-full hover:after:left-0 flex items-center gap-2">          
+        <FaBlogger className="text-lg"/>Blog</a>
 
         <DarkTheme />
 
