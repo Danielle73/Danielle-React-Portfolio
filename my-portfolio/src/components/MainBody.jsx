a5266f2d71d42b29a5091b0a912ab8b788809d00
Danielle Lee <daniellelee73@gmail.com>
2025-07-06 20:30:03 +0100

    Changes made to update domain for accessibility blog.

--- my-portfolio/src/components/MainBody.jsx
+++ my-portfolio/src/components/MainBody.jsx
@@ -1,40 +1,64 @@
-import ProfilePicture from '../assets/ProfilePicture.png'; 
-function MainBody(){
-
-    return (
-      <div id="home">
+import ProfilePicture from "../assets/ProfilePicture.png";
+function MainBody() {
+  return (
+    <div id="home">
       <div className="TopContainer flex justify-evenly mt-28 mb-28">
-      <div className="LeftContainer m-3">
-        <h2>Hello I&apos;m <br/> Danielle</h2>
-        <div className="aboutMeParagraph text-4xl"> 
-          <p>I&apos;m a Front-End Developer <br /> based in London.</p>
-          <p>Nice to meet you!</p>
+        <div className="LeftContainer m-3">
+          <h2>
+            Hello I&apos;m <br /> Danielle
+          </h2>
+          <div className="aboutMeParagraph text-4xl">
+            <p>
+              I&apos;m a Front-End Developer <br /> based in London.
+            </p>
+            <p>Nice to meet you!</p>
+          </div>
+          <br />
+          <button
+            className="custom-button mr-2"
+            onClick={() =>
+              window.open(
+                "https://www.linkedin.com/in/danielle-lee-ab8b0887/",
+                "_blank"
+              )
+            }
+          >
+            LinkedIn
+          </button>
+
+          <button
+            className="custom-button mr-2"
+            onClick={() =>
+              window.open(
+                "https://docs.google.com/document/d/1PpI2Y89l0L2hDfeeFyddD49pbMMatgF9/edit?usp=drive_link&ouid=102033806727803085093&rtpof=true&sd=true",
+                "_blank"
+              )
+            }
+          >
+            C.V.
+          </button>
+
+          <button
+            className="custom-button"
+            onClick={() =>
+              window.open("https://www.skiptocontent.co.uk/", "_blank")
+            }
+          >
+            Accessibility Blog
+          </button>
         </div>
-        <br />
-        <button className="custom-button mr-2" 
-        onClick={() => window.open("https://www.linkedin.com/in/danielle-lee-ab8b0887/", "_blank")}
-        >LinkedIn</button> 
 
-        <button className="custom-button mr-2"
-          onClick={() => window.open('https://docs.google.com/document/d/1PpI2Y89l0L2hDfeeFyddD49pbMMatgF9/edit?usp=drive_link&ouid=102033806727803085093&rtpof=true&sd=true', '_blank')}
-        >C.V.</button>
-
-         <button className="custom-button"
-          onClick={() => window.open('https://accessibility-blog.vercel.app/', '_blank')}
-        >Accessibility Blog</button>
-      </div>
-
-     <div className="RightContainer relative m-3 mt-8">
-        <img 
-          src={ProfilePicture} 
-          alt="Profile Picture" 
-          className="rounded-full border-8 border-yellow-400"
-          width={350}
-        />
+        <div className="RightContainer relative m-3 mt-8">
+          <img
+            src={ProfilePicture}
+            alt="Profile Picture"
+            className="rounded-full border-8 border-yellow-400"
+            width={350}
+          />
+        </div>
       </div>
     </div>
-    </div>
-    )
+  );
 }
 
 export default MainBody;
--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -4,35 +4,57 @@ import { FaBlogger, FaRegFolder } from "react-icons/fa6";
 import { FaRegAddressCard } from "react-icons/fa";
 import { FaRegEnvelope } from "react-icons/fa6";
 import { BsHouseHeart } from "react-icons/bs";
-import DarkTheme from './DarkMode';
-
+import DarkTheme from "./DarkMode";
 
 const Navbar = () => {
-  return ( 
+  return (
     <>
       <div className="NavContainer flex justify-evenly shadow-md pt-10 pb-10">
         {/* Home Link */}
-        <a href="#home" className="mr-96 flex items-center gap-2 relative px-4 py-3 after:block after:absolute after:bottom-0 after:left-1/2 after:w-0 after:h-0.5 after:bg-yellow-400 after:transition-all after:duration-300 hover:after:w-full hover:after:left-0">          
-        <BsHouseHeart className="text-lg" />Danielle Lee</a>
+        <a
+          href="#home"
+          className="mr-96 flex items-center gap-2 relative px-4 py-3 after:block after:absolute after:bottom-0 after:left-1/2 after:w-0 after:h-0.5 after:bg-yellow-400 after:transition-all after:duration-300 hover:after:w-full hover:after:left-0"
+        >
+          <BsHouseHeart className="text-lg" />
+          Danielle Lee
+        </a>
 
         {/* Other Links */}
-        <a href="#about" className="relative px-4 py-3 after:block after:absolute after:bottom-0 after:left-1/2 after:w-0 after:h-0.5 after:bg-yellow-400 after:transition-all after:duration-300 hover:after:w-full hover:after:left-0 flex items-center gap-2">          
-        <FaRegAddressCard className="text-lg" />About</a>
+        <a
+          href="#about"
+          className="relative px-4 py-3 after:block after:absolute after:bottom-0 after:left-1/2 after:w-0 after:h-0.5 after:bg-yellow-400 after:transition-all after:duration-300 hover:after:w-full hover:after:left-0 flex items-center gap-2"
+        >
+          <FaRegAddressCard className="text-lg" />
+          About
+        </a>
 
-        <a href="#projects" className="relative px-4 py-3 after:block after:absolute after:bottom-0 after:left-1/2 after:w-0 after:h-0.5 after:bg-yellow-400 after:transition-all after:duration-300 hover:after:w-full hover:after:left-0 flex items-center gap-2">          
-        <FaRegFolder className="text-lg" />Projects</a>
+        <a
+          href="#projects"
+          className="relative px-4 py-3 after:block after:absolute after:bottom-0 after:left-1/2 after:w-0 after:h-0.5 after:bg-yellow-400 after:transition-all after:duration-300 hover:after:w-full hover:after:left-0 flex items-center gap-2"
+        >
+          <FaRegFolder className="text-lg" />
+          Projects
+        </a>
 
-        <a href="#contact" className="relative px-4 py-3 after:block after:absolute after:bottom-0 after:left-1/2 after:w-0 after:h-0.5 after:bg-yellow-400 after:transition-all after:duration-300 hover:after:w-full hover:after:left-0 flex items-center gap-2">          
-          <FaRegEnvelope className="text-lg"/>Contact</a>
+        <a
+          href="#contact"
+          className="relative px-4 py-3 after:block after:absolute after:bottom-0 after:left-1/2 after:w-0 after:h-0.5 after:bg-yellow-400 after:transition-all after:duration-300 hover:after:w-full hover:after:left-0 flex items-center gap-2"
+        >
+          <FaRegEnvelope className="text-lg" />
+          Contact
+        </a>
 
-        <a href="https://accessibility-blog.vercel.app/" target="_blank" className="relative px-4 py-3 after:block after:absolute after:bottom-0 after:left-1/2 after:w-0 after:h-0.5 after:bg-yellow-400 after:transition-all after:duration-300 hover:after:w-full hover:after:left-0 flex items-center gap-2">          
-        <FaBlogger className="text-lg"/>Blog</a>
+        <a
+          href="https://www.skiptocontent.co.uk/"
+          target="_blank"
+          className="relative px-4 py-3 after:block after:absolute after:bottom-0 after:left-1/2 after:w-0 after:h-0.5 after:bg-yellow-400 after:transition-all after:duration-300 hover:after:w-full hover:after:left-0 flex items-center gap-2"
+        >
+          <FaBlogger className="text-lg" />
+          Blog
+        </a>
 
         <DarkTheme />
-
       </div>
-      
-
     </>
   );
 };
