a50fa7c5f62b01e8b01cc332f82678875b747bd7
Danielle Lee <daniellelee73@gmail.com>
2025-09-30 03:07:10 +0100

    text color changed to increase contrast due to accesibility issues

--- my-portfolio/src/pages/Homepage.jsx
+++ my-portfolio/src/pages/Homepage.jsx
@@ -4,22 +4,22 @@ const Homepage = () => {
         <div className="mainContainer flex flex-col justify-center items-center p-3 space-y-4">
 
         {/* about container */}
-        <div className="aboutContainer text-wrap flex-col p-8 ml-36 mr-36 ">
+        <div className="aboutContainer text-wrap flex-col p-8 ml-36 mr-36">
 
-        <p>
+        <p className="text-gray-700">
           Hi, I&apos;m Danielle, a Front-End Developer with a strong foundation in React, TypeScript, and JavaScript. 
           After completing the Founders & Coders bootcamp, I&apos;ve continued refining my skills through personal projects, 
           technical challenges, and continuous learning.
           </p>
           <br />
-          <p>
+          <p className="text-gray-700">
             With a background in marketing and customer service, I bring a user-focused approach to web development
             alongside a growing specialisation in web accessibility and inclusive design. 
             I&apos;m committed to learning and implementing WCAG guidelines, semantic HTML, and accessible design patterns 
             to create better experiences for all users.
             </p>
           <br /> 
-        <p>Beyond tech, I&apos;m a travel enthusiast, a puzzle lover, and an avid language learner (thanks to Duolingo&apos;s duress).</p>
+        <p className="text-gray-700">Beyond tech, I&apos;m a travel enthusiast, a puzzle lover, and an avid language learner (thanks to Duolingo&apos;s duress).</p>
     
         </div>
       </div>
